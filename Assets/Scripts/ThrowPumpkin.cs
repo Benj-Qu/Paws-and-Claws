14f0acc78204ae98615e639b7ede98d4026a69b3
Yibei Han <hanyibei@umich.edu>
2023-03-12 03:16:48 -0400

    collider fixed, scarecrow fixed

--- Assets/Scripts/ThrowPumpkin.cs
+++ Assets/Scripts/ThrowPumpkin.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ThrowPumpkin : MonoBehaviour
 {
     public GameObject Pumpkin;
+    public GameObject Fork;
 
-    private float speed = 3;
+    private float speed = 4;
+    private int seed;
     // Start is called before the first frame update
 
     void Start()
@@ -23,9 +25,17 @@ public class ThrowPumpkin : MonoBehaviour
 
     void pumpkinThrow()
     {
+        seed = Random.Range(0, 2);
         Vector3 pos = this.transform.position;
-        pos += new Vector3(0, 1.5f, 0);
-        GameObject pump = Instantiate(Pumpkin, pos, Quaternion.identity);
+        //pos += new Vector3(0, 1.5f, 0);
+        GameObject pump;
+        if (seed == 1)
+        {
+            pump = Instantiate(Pumpkin, pos, Quaternion.identity);
+        } else
+        {
+            pump = Instantiate(Fork, pos, Quaternion.identity);
+        }
 
         Rigidbody2D rgbd = pump.GetComponent<Rigidbody2D>();
         rgbd.velocity = new Vector2(speed, 0);
@@ -35,9 +45,20 @@ public class ThrowPumpkin : MonoBehaviour
 
     void pumpkinThrow2()
     {
+        seed = Random.Range(0, 2);
         Vector3 pos = this.transform.position;
-        pos += new Vector3(0, 1.5f, 0);
-        GameObject pump = Instantiate(Pumpkin, pos, Quaternion.identity);
+        //pos += new Vector3(0, 1.5f, 0);
+        GameObject pump;
+        if (seed == 1)
+        {
+            pump = Instantiate(Pumpkin, pos, Quaternion.identity);
+        }
+        else
+        {
+            Quaternion rotate = Quaternion.identity;
+            rotate.eulerAngles += new Vector3(0, 180, 0);
+            pump = Instantiate(Fork, pos, rotate);
+        }
 
         Rigidbody2D rgbd = pump.GetComponent<Rigidbody2D>();
         rgbd.velocity = new Vector2(-speed, 0);
