d31c9e392847356b531eac3e8f243fd65058c8c2
Yibei Han <hanyibei@umich.edu>
2023-03-12 15:46:56 -0400

    bomb

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -6,6 +6,7 @@ public class BombController : MonoBehaviour
 {
     private GameObject explosionAes;
     private bool explosion = false;
+    private bool start = false;
     
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,15 @@ public class BombController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       if (!start && GameController.instance.stage == 2)
+        {
+            start = true;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Block"))
+        if (collision.CompareTag("Block") && start)
         {
             blockMovement bm1 = collision.gameObject.GetComponent<blockMovement>();
             blockMovement bm2 = gameObject.GetComponent<blockMovement>();
@@ -40,7 +44,7 @@ public class BombController : MonoBehaviour
     
     void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Block"))
+        if (collision.CompareTag("Block") && start)
         {
             blockMovement bm1 = collision.gameObject.GetComponent<blockMovement>();
             blockMovement bm2 = gameObject.GetComponent<blockMovement>();
