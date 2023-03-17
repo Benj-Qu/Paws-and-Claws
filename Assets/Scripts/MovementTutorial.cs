69e730596ef48ff430cc3cea918d2fd7b874e584
Yibei Han <hanyibei@umich.edu>
2023-03-16 23:24:39 -0400

    control tutorial

--- Assets/Scripts/MovementTutorial.cs
+++ Assets/Scripts/MovementTutorial.cs
@@ -6,16 +6,62 @@ public class MovementTutorial : MonoBehaviour
 {
     public GameObject FlagRight;
     public GameObject FlagLeft;
+    public GameObject JumpBox1;
+    public GameObject JumpBox2;
+    public GameObject WallBox1;
+    public GameObject WallBox2;
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject HintText2;
+    public int target = 0;
+
+
+    // 1: basic, 2: jump, 3: wall jump, 4: finished;
+    private int status = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        FlagLeft.transform.position = new Vector3(-1f, -1.1f, 0);
+        FlagRight.transform.position = new Vector3(3f, -1.1f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == 2 && status == 1)
+        {
+            JumpBox1.SetActive(true);
+            JumpBox2.SetActive(true);
+            Player1.transform.position = new Vector3(-6f, -2.2f, 0);
+            Player2.transform.position = new Vector3(6f, -2.2f, 0);
+            FlagLeft.transform.position = new Vector3(0, -0.2f, 0);
+            FlagLeft.GetComponent<TutorialFlag>().touched = false;
+            FlagRight.transform.position = new Vector3(2f, -0.2f, 0);
+            FlagRight.GetComponent<TutorialFlag>().touched = false;
+            target = 0;
+            status += 1;
+        }
+        if (target == 2 && status == 2)
+        {
+            HintText2.SetActive(true);
+            JumpBox1.SetActive(false);
+            JumpBox2.SetActive(false);
+            WallBox1.SetActive(true);
+            WallBox2.SetActive(true);
+            Player1.transform.position = new Vector3(-6f, -2.2f, 0);
+            Player2.transform.position = new Vector3(6f, -2.2f, 0);
+            FlagLeft.transform.position = new Vector3(0.25f, 1f, 0);
+            FlagLeft.GetComponent<TutorialFlag>().touched = false;
+            FlagRight.transform.position = new Vector3(1.75f, -0.2f, 0);
+            FlagRight.GetComponent<TutorialFlag>().touched = false;
+            target = 0;
+            status += 1;
+        }
+        if (target == 2 && status == 3)
+        {
+            target = 0;
+            status = 1;
+        }
     }
 }
---
+++ Assets/Scripts/TutorialFlag.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFlag : MonoBehaviour
+{
+    public MovementTutorial mt;
+    public bool touched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            mt.target += 1;
+            touched = true;
+        }
+    }
+}
