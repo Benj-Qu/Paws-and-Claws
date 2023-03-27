90fe04a1dded114fe3e0718b0330cf4dc8cfdb96
Yibei Han <hanyibei@umich.edu>
2023-03-27 01:23:33 -0400

    level0

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -64,7 +64,7 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        Screen.SetResolution(1600, 1000, false);
+        //Screen.SetResolution(1600, 1000, false);
         level = SceneManager.GetActiveScene().name;
         // scene = SceneManager.GetActiveScene().name[7] - '0';
         // scene = 0;
