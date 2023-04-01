8e07a2b3cc7200ab5c640877603e1cf1df18806e
YIYING JIANG <mindyjyy@YIYINGs-MacBook-Pro.local>
2023-04-01 02:21:05 -0400

    finish story

--- Assets/Scripts/textController.cs
+++ Assets/Scripts/textController.cs
@@ -15,15 +15,11 @@ public class textController : MonoBehaviour
         if (level == "Story")
         {
             scripts.Enqueue("[speed=0.1]<b>Guadians are strong. They can climb high walls!</b>");
-            scripts.Enqueue("我从2015年开始在CSDN写博客，到现在有近[speed=0.2]<size=40>两万粉丝</size>啦。");
-            scripts.Enqueue("感谢大家的[speed=0.3]<size=40>关注</size>与<b>支持</b>。");
-            scripts.Enqueue("我会持续输出Unity[speed=0.3]干货文章[speed=0.1]，希望可以帮助到想学Unity的同学，共勉!");
-            scripts.Enqueue("好了，那么我们下一篇文章再见，[speed=0.4]<i>拜拜</i>。");
-
-            while(scripts.Count >= 0)
-            {
-                ShowScript();
-            }
+            //scripts.Enqueue("我从2015年开始在CSDN写博客，到现在有近[speed=0.2]<size=40>两万粉丝</size>啦。");
+            //scripts.Enqueue("感谢大家的[speed=0.3]<size=40>关注</size>与<b>支持</b>。");
+            //scripts.Enqueue("我会持续输出Unity[speed=0.3]干货文章[speed=0.1]，希望可以帮助到想学Unity的同学，共勉!");
+            //scripts.Enqueue("好了，那么我们下一篇文章再见，[speed=0.4]<i>拜拜</i>。");
+            ShowScript();
         }
     }
 
@@ -31,14 +27,16 @@ public class textController : MonoBehaviour
     {
         if (scripts.Count <= 0)
         {
-            if(level == "Story")
+            return;
+        }
+        text.TypeText(scripts.Dequeue(), onComplete: () => {
+            Debug.Log("TypeText Complete");
+            if (level == "Story" && scripts.Count == 0)
             {
                 SceneManager.LoadScene("NewIntro");
             }
-            return;
-        }
-
-        text.TypeText(scripts.Dequeue(), onComplete: () => Debug.Log("TypeText Complete"));
+        });
+        
     }
 
     public void updateText(string text)
