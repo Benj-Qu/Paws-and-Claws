100964cfa7bc8c4420387e71b592c210065e3daf
Benj-Qu <quzhemin@umich.com>
2023-04-07 14:40:25 -0400

    BlockInfo

---
+++ Assets/Scripts/BlockUtils/BlockInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Length = 1;
+    public int Width = 1;
+    public Vector2 BornShift = Vector2.zero;
+}
