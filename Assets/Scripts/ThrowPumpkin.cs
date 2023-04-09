252267167c3f137264213ae7af73d0e2e4a919b5
Benj-Qu <quzhemin@umich.com>
2023-04-08 20:44:53 -0400

    Penguin Attack Animation

--- Assets/Scripts/ThrowPumpkin.cs
+++ Assets/Scripts/ThrowPumpkin.cs
@@ -7,6 +7,9 @@ public class ThrowPumpkin : MonoBehaviour
     public GameObject Pumpkin;
     public GameObject Fork;
     public bool left = true;
+    public float RepeatRate = 1.5f;
+    public float AttackDist = 1.5f;
+    public bool isPenguin = false;
 
     private float speed = 4;
     private int seed;
@@ -27,8 +30,8 @@ public class ThrowPumpkin : MonoBehaviour
         if (start == false && GameController.instance.stage == 2)
         {
             start = true;
-            InvokeRepeating("pumpkinThrow", 2f, 3f);
-            InvokeRepeating("pumpkinThrow2", 3.5f, 3f);
+            InvokeRepeating("pumpkinThrow1", 2f, 2 * RepeatRate);
+            InvokeRepeating("pumpkinThrow2", 2f + RepeatRate, 2 * RepeatRate);
         }
 
         if (start == true && GameController.instance.stage != 2)
@@ -38,11 +41,16 @@ public class ThrowPumpkin : MonoBehaviour
         }
     }
 
-    void pumpkinThrow()
+    void pumpkinThrow1()
     {
+        if (isPenguin)
+        {
+            StartCoroutine(PenguinAttack1());
+            return;
+        }
         seed = Random.Range(0, 2);
         Vector3 pos = this.transform.position;
-        pos += new Vector3(1.5f, 0, 0);
+        pos += new Vector3(AttackDist, 0, 0);
         GameObject pump;
         if (left == false)
         {
@@ -64,9 +72,14 @@ public class ThrowPumpkin : MonoBehaviour
 
     void pumpkinThrow2()
     {
+        if (isPenguin)
+        {
+            StartCoroutine(PenguinAttack2());
+            return;
+        }
         seed = Random.Range(0, 2);
         Vector3 pos = this.transform.position;
-        pos += new Vector3(-1.5f, 0, 0);
+        pos += new Vector3(-AttackDist, 0, 0);
         GameObject pump;
         if (left == true)
         {
@@ -86,6 +99,44 @@ public class ThrowPumpkin : MonoBehaviour
             pump = Instantiate(Fork, pos, rotate);
         }
 
+        Rigidbody2D rgbd = pump.GetComponent<Rigidbody2D>();
+        rgbd.velocity = new Vector2(-speed, 0);
+        rgbd.gravityScale = 0;
+    }
+
+    private IEnumerator PenguinAttack1()
+    {
+        Vector3 pos = this.transform.position;
+        pos += new Vector3(AttackDist, 0, 0);
+        GameObject pump;
+        if (left == false)
+        {
+            left = !left;
+            sr.flipX = !sr.flipX;
+        }
+        GetComponent<Animator>().SetTrigger("Attack");
+        yield return new WaitForSeconds(15f / 60f);
+        pump = Instantiate(Pumpkin, pos, Quaternion.identity);
+        Rigidbody2D rgbd = pump.GetComponent<Rigidbody2D>();
+        rgbd.velocity = new Vector2(speed, 0);
+        rgbd.gravityScale = 0;
+    }
+
+    private IEnumerator PenguinAttack2()
+    {
+        Vector3 pos = this.transform.position;
+        pos += new Vector3(-AttackDist, 0, 0);
+        GameObject pump;
+        if (left == true)
+        {
+            left = !left;
+            sr.flipX = !sr.flipX;
+        }
+        GetComponent<Animator>().SetTrigger("Attack");
+        yield return new WaitForSeconds(15f / 60f);
+        Quaternion rotate = Quaternion.identity;
+        rotate.eulerAngles += new Vector3(0, 180, 0);
+        pump = Instantiate(Fork, pos, rotate);
         Rigidbody2D rgbd = pump.GetComponent<Rigidbody2D>();
         rgbd.velocity = new Vector2(-speed, 0);
         rgbd.gravityScale = 0;
